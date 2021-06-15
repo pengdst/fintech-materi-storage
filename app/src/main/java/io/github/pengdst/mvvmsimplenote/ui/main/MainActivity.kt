0ba73ccd62a243df5232217ee91fbccf6046ce08
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-15 16:46:00 +0700

    test get note

--- app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/main/MainActivity.kt
@@ -2,11 +2,35 @@ package io.github.pengdst.mvvmsimplenote.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import io.github.pengdst.mvvmsimplenote.R
+import io.github.pengdst.mvvmsimplenote.data.db.NoteDatabase
+import io.github.pengdst.mvvmsimplenote.data.db.model.NoteEntity
+import io.github.pengdst.mvvmsimplenote.data.network.RetrofitBuilder
+import io.github.pengdst.mvvmsimplenote.data.network.service.NoteService
+import io.github.pengdst.mvvmsimplenote.data.repository.NoteRepository
+import io.github.pengdst.mvvmsimplenote.ui.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val noteDao = NoteDatabase.newInstance(this).noteDao()
+        val apiService = RetrofitBuilder.newInstance().create(NoteService::class.java)
+        val repository = NoteRepository(noteDao, apiService)
+        val factory = ViewModelFactory(repository)
+
+        mainViewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
+
+        mainViewModel.getAllNotes().observe(this) {
+            Log.d("TAG", "Notes: $it")
+        }
+
+        mainViewModel.insert(NoteEntity(0, "Nama"))
     }
 }
