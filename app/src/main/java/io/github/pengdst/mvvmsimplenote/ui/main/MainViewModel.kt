877ef950005dcfa099d9674fd7d964f284066626
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-15 16:45:46 +0700

    create postOnlineNote and getOnlineNote in viewmodel

--- app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/main/MainViewModel.kt
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/main/MainViewModel.kt
@@ -1,8 +1,11 @@
 package io.github.pengdst.mvvmsimplenote.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.github.pengdst.mvvmsimplenote.data.db.model.NoteEntity
+import io.github.pengdst.mvvmsimplenote.data.network.response.NoteDto
 import io.github.pengdst.mvvmsimplenote.data.repository.NoteRepository
 import kotlinx.coroutines.launch
 
@@ -13,13 +16,41 @@ import kotlinx.coroutines.launch
  * - Gitlab https://gitlab.com/pengdst
  * - LinkedIn https://linkedin.com/in/pengdst
  */
-class MainViewModel(private val noteRepository: NoteRepository) : ViewModel() {
+class MainViewModel(private val repository: NoteRepository) : ViewModel() {
 
-    fun getAllNotes() = noteRepository.getAllNotes()
+    fun getAllNotes() = repository.getAllNotes()
 
-    fun deleteNote(noteEntity: NoteEntity) {
+    fun getOnlineNote(): LiveData<List<NoteDto>> {
+        val liveData = MutableLiveData<List<NoteDto>>()
         viewModelScope.launch {
-            noteRepository.deleteNote(noteEntity)
+            liveData.postValue(repository.getOnlineNotes())
+        }
+        return liveData
+    }
+
+    fun postOnlineNote(noteDto: NoteDto) : LiveData<NoteDto> {
+        val liveData = MutableLiveData<NoteDto>()
+        viewModelScope.launch {
+            liveData.postValue(repository.createOnlineNote(noteDto))
+        }
+        return liveData
+    }
+
+    fun delete(entity: NoteEntity) {
+        viewModelScope.launch {
+            repository.deleteNote(entity)
+        }
+    }
+
+    fun insert(entity: NoteEntity) {
+        viewModelScope.launch {
+            repository.insertNote(entity)
+        }
+    }
+
+    fun update(entity: NoteEntity) {
+        viewModelScope.launch {
+            repository.updateNote(entity)
         }
     }
 
