42546a0a96ee6143e9578da8a22cf18914db050a
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-15 16:42:17 +0700

    inject noteService

--- app/src/main/java/io/github/pengdst/mvvmsimplenote/data/repository/NoteRepository.kt
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/data/repository/NoteRepository.kt
@@ -1,7 +1,9 @@
 package io.github.pengdst.mvvmsimplenote.data.repository
 
-import io.github.pengdst.mvvmsimplenote.data.db.NoteDatabase
+import io.github.pengdst.mvvmsimplenote.data.db.dao.NoteDao
 import io.github.pengdst.mvvmsimplenote.data.db.model.NoteEntity
+import io.github.pengdst.mvvmsimplenote.data.network.response.NoteDto
+import io.github.pengdst.mvvmsimplenote.data.network.service.NoteService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -13,11 +15,34 @@ import kotlinx.coroutines.withContext
  * - LinkedIn https://linkedin.com/in/pengdst
  */
 
-class NoteRepository(private val db: NoteDatabase) {
-
-    private val noteDao = db.noteDao()
+class NoteRepository(
+    private val noteDao: NoteDao,
+    private val noteService: NoteService
+    ) {
 
     fun getAllNotes() = noteDao.getAllNotes()
+
+    suspend fun getOnlineNotes(): List<NoteDto> {
+        return withContext(Dispatchers.IO) {
+            val response = noteService.getAllNotes()
+            try {
+                response.body()?.data ?: emptyList()
+            } catch (e: Exception) {
+                emptyList()
+            }
+        }
+    }
+
+    suspend fun createOnlineNote(noteDto: NoteDto): NoteDto? {
+        return withContext(Dispatchers.IO) {
+            val response = noteService.createNote(noteDto)
+            try {
+                response.body()?.data
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
 
     suspend fun insertNote(noteEntity: NoteEntity) = withContext(Dispatchers.IO) {
         noteDao.insert(noteEntity)
