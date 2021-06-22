fedc5b3d6f8b1f66c01d99b3b7660d100aa4bcce
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-22 12:29:57 +0700

    add button open messaging activity

--- app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package io.github.pengdst.mvvmsimplenote.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
 import io.github.pengdst.mvvmsimplenote.R
 import io.github.pengdst.mvvmsimplenote.data.db.NoteDatabase
@@ -11,14 +13,18 @@ import io.github.pengdst.mvvmsimplenote.data.network.RetrofitBuilder
 import io.github.pengdst.mvvmsimplenote.data.network.service.NoteService
 import io.github.pengdst.mvvmsimplenote.data.repository.NoteRepository
 import io.github.pengdst.mvvmsimplenote.ui.ViewModelFactory
+import io.github.pengdst.mvvmsimplenote.ui.messaging.MessagingActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel: MainViewModel
+    private lateinit var btnOpenMessaging: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOpenMessaging = findViewById(R.id.btn_open_messaging)
 
         val noteDao = NoteDatabase.newInstance(this).noteDao()
         val apiService = RetrofitBuilder.newInstance().create(NoteService::class.java)
@@ -32,5 +38,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         mainViewModel.insert(NoteEntity(0, "Nama"))
+
+        btnOpenMessaging.setOnClickListener {
+            val intent = Intent(applicationContext, MessagingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
