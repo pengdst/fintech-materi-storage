c05108f5ee7625f64bb7dc0d7f97de01d010359a
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-22 11:08:55 +0700

    cleanup code

--- app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/messaging/MessagingActivity.kt
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/messaging/MessagingActivity.kt
@@ -1,13 +1,7 @@
 package io.github.pengdst.mvvmsimplenote.ui.messaging
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.ktx.messaging
 import io.github.pengdst.mvvmsimplenote.R
 
 class MessagingActivity : AppCompatActivity() {
