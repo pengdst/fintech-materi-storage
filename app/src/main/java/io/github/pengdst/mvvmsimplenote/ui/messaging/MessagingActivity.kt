82d806cff1f423d45571335f866211f257cb7ebe
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-22 10:54:15 +0700

    get device token

--- app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/messaging/MessagingActivity.kt
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/messaging/MessagingActivity.kt
@@ -1,13 +1,32 @@
 package io.github.pengdst.mvvmsimplenote.ui.messaging
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
 import io.github.pengdst.mvvmsimplenote.R
 
 class MessagingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messaging)
+
+        Firebase.messaging.token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e("MessagingToken", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            val token = task.result
+            val msg = "Device token: ${ token.toString() }"
+
+            Log.e("MessagingToken", msg)
+            Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+        })
 
     }
 }
