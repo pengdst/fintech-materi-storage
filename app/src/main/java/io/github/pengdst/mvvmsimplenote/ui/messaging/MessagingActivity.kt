7513d3ab35429e767b9bed4f60c02ed46c18d3d1
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-22 10:50:26 +0700

    create messaging activity

---
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/messaging/MessagingActivity.kt
@@ -0,0 +1,13 @@
+package io.github.pengdst.mvvmsimplenote.ui.messaging
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.pengdst.mvvmsimplenote.R
+
+class MessagingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messaging)
+
+    }
+}
