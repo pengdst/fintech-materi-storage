2d36207d7a31a3927815fc529e2e2b4929e449da
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-29 16:39:36 +0700

    ViewModelFactory.kt support multi viewmodel

--- app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/ViewModelFactory.kt
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/ViewModelFactory.kt
@@ -2,9 +2,11 @@ package io.github.pengdst.mvvmsimplenote.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.github.pengdst.mvvmsimplenote.data.repository.BaseRepository
+import io.github.pengdst.mvvmsimplenote.data.repository.FirestoreNoteRepository
 import io.github.pengdst.mvvmsimplenote.data.repository.NoteRepository
+import io.github.pengdst.mvvmsimplenote.ui.firestore.FirestoreViewModel
 import io.github.pengdst.mvvmsimplenote.ui.main.MainViewModel
-import java.lang.ClassCastException
 
 /**
  * Created on 6/7/21 by Pengkuh Dwi Septiandi (@pengdst)
@@ -13,15 +15,19 @@ import java.lang.ClassCastException
  * - Gitlab https://gitlab.com/pengdst
  * - LinkedIn https://linkedin.com/in/pengdst
  */
-class ViewModelFactory(private val noteRepository: NoteRepository) : ViewModelProvider.Factory {
+class ViewModelFactory(private val repostories: BaseRepository) :
+    ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return if (modelClass.isAssignableFrom(MainViewModel::class.java)){
-            MainViewModel(noteRepository)
+        return when {
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                MainViewModel(repostories as NoteRepository) as T
+            }
+            modelClass.isAssignableFrom(FirestoreViewModel::class.java) -> {
+                FirestoreViewModel(repostories as FirestoreNoteRepository) as T
+            }
+            else -> throw ClassCastException("Cannot provide ${modelClass.name}")
         }
-        else {
-            throw ClassCastException("Cannot provide ${modelClass.name}")
-        } as T
     }
 }
