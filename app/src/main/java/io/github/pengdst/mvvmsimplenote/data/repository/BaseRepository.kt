83013e55c0cab671fcecc3854c9c2b41b55e285f
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-29 16:37:44 +0700

    membuat BaseRepository.kt

---
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/data/repository/BaseRepository.kt
@@ -0,0 +1,10 @@
+package io.github.pengdst.mvvmsimplenote.data.repository
+
+/**
+ * Created on 6/29/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+abstract class BaseRepository
