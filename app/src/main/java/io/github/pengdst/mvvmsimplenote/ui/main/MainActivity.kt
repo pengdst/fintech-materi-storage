93f5875ba6002a2f82a837a37887539726384715
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-29 18:00:41 +0700

    button open firestore activity

--- app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/mvvmsimplenote/ui/main/MainActivity.kt
@@ -13,18 +13,21 @@ import io.github.pengdst.mvvmsimplenote.data.network.RetrofitBuilder
 import io.github.pengdst.mvvmsimplenote.data.network.service.NoteService
 import io.github.pengdst.mvvmsimplenote.data.repository.NoteRepository
 import io.github.pengdst.mvvmsimplenote.ui.ViewModelFactory
+import io.github.pengdst.mvvmsimplenote.ui.firestore.FirestoreActivity
 import io.github.pengdst.mvvmsimplenote.ui.messaging.MessagingActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel: MainViewModel
     private lateinit var btnOpenMessaging: Button
+    private lateinit var btnOpenFirestore: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnOpenMessaging = findViewById(R.id.btn_open_messaging)
+        btnOpenFirestore = findViewById(R.id.btn_open_firestore)
 
         val noteDao = NoteDatabase.newInstance(this).noteDao()
         val apiService = RetrofitBuilder.newInstance().create(NoteService::class.java)
@@ -41,6 +44,11 @@ class MainActivity : AppCompatActivity() {
 
         btnOpenMessaging.setOnClickListener {
             val intent = Intent(applicationContext, MessagingActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnOpenFirestore.setOnClickListener {
+            val intent = Intent(applicationContext, FirestoreActivity::class.java)
             startActivity(intent)
         }
     }
